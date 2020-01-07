7e3aaba06c1e58914f002b66c0eeda4b20c9f5ba
Kevin Milner <kmilner@usc.edu>
2020-01-06 16:20:04 -0800

    made ETAS_MEM_GB work with fractions

--- sbin/u3etas_jar_wrapper.sh
+++ sbin/u3etas_jar_wrapper.sh
@@ -5,11 +5,18 @@ set -o errexit
 # this is a utility script for jaunching a java process with the USCERF3-ETAS jar file in the classpath
 # will be called by other scripts
 
-# maxmimum memory in gigabytes. should be close to, but not over, total memory available
+# maxmimum memory. should be close to, but not over, total memory available
 # can set externally with ETAS_MEM_GB environmental variable
 if [[ ! -z "$ETAS_MEM_GB" ]];then
-	MEM_GIGS=$ETAS_MEM_GB
-	echo "Using global ETAS_MEM_GB=$ETAS_MEM_GB"
+	if ! [[ "$ETAS_MEM_GB" =~ ^[0-9]+$ ]];then
+	        # it's a fraction, convert to megabytes
+		MEM_MB=`bc <<< $ETAS_MEM_GB*1024/1`
+		echo "Converting fractional ETAS_MEM_GB to megabytes: $ETAS_MEM_GB GB = $MEM_MB MB"
+		MEM_ARG="-Xmx${MEM_MB}M"
+	else
+		echo "Using global ETAS_MEM_GB=$ETAS_MEM_GB"
+		MEM_ARG="-Xmx${ETAS_MEM_GB}G"
+	fi
 else
 	echo "ETAS_MEM_GB is not set, will automatically detect maximum memory as 80% of total system memory"
 	TOT_MEM=`free | grep Mem | awk '{print $2}'`
@@ -17,9 +24,10 @@ else
 	TARGET_MEM_MB=`expr $TOT_MEM_MB \* 8 / 10`
 	MEM_GIGS=`expr $TARGET_MEM_MB / 1024`
 	echo "     will use up to $MEM_GIGS GB of memory"
+	MEM_ARG="-Xmx{MEM_GIGS}G"
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )/../"
 
-java -Xmx${MEM_GIGS}G -cp $DIR/lib/opensha-ucerf3-all.jar $@
+java $MEM_ARG -cp $DIR/lib/opensha-ucerf3-all.jar $@
 exit $?
