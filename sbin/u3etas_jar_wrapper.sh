933cee334d064ea5053cdec7dcb53df7796e46f4
Kevin Milner <kmilner@usc.edu>
2021-05-17 14:33:48 -0700

    updated scrips to use new jar

--- sbin/u3etas_jar_wrapper.sh
+++ sbin/u3etas_jar_wrapper.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # this is a utility script for jaunching a java process with the USCERF3-ETAS jar file in the classpath
-# will be called by other scripts
+# it will be called by other scripts
 
 # maxmimum memory. should be close to, but not over, total memory available
 # can set externally with ETAS_MEM_GB environmental variable
@@ -29,5 +29,8 @@ fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )/../"
 
-java -Djava.awt.headless=true $MEM_ARG -cp $DIR/lib/opensha-ucerf3-all.jar $@
+# check for updates
+$DIR/opensha/check_dl_update_build.sh
+
+java -Djava.awt.headless=true $MEM_ARG -cp $DIR/opensha/opensha-all.jar $@
 exit $?
